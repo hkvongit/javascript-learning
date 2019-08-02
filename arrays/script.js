a829f319da64ee590900e66c3682fcfa03558d9c
hkvongitlab <brosgiant@gmail.com>
2019-08-02 23:23:58 +0530

    arrays

---
+++ arrays/script.js
@@ -0,0 +1,14 @@
+var name=["john", "hari", "rumi"]
+// name.splice(1,2) // remove 2 elements from position 1 and onwards
+// name.push("rintu") //add an eleent to the back
+// name.pop() //removes last element
+// name.shift() // removes the first element 
+// console.log(name.ind  exOf("hari"))  // returns the index of element "hari"
+const a="rumi"
+if (!name.indexOf(a)===1){
+    console.log(`no ${a}`)
+}else{
+    console.log(` ${a} is here`)
+}
+
+console.log(name)
---
+++ part1/variable mutation.js
@@ -0,0 +1,6 @@
+var name="john"
+var age=23
+console.log(name,age)
+
+var {nameA, nameB} ={"harikrishnan","sreesankar"}
+console.log(nameA,nameB)
