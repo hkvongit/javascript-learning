807bbaba22705da0d650c5e9c8b4bffab12fd467
hkvongit <brosgiant@gmail.com>
2021-07-04 23:35:07 +0530

    Function to getcookie after parsing

--- cookies/cookies.js
+++ cookies/cookies.js
@@ -1,16 +1,42 @@
+// /**
+//  * Setting cookie function
+//  * @param {*} key 
+//  * @param {*} value 
+//  * @param {*} lifeInSeconds 
+//  */
+// const setCookie = (key, value, lifeInSeconds) => {
+//   const date = new Date();
+//   const expiryTime = (date.getTime() + lifeInSeconds);
+//   date.setTime(expiryTime);
+//   const cookieString = `${key}=${value};expires=${date.toGMTString()}`;
+//   console.log("cookieString", cookieString)
+//   document.cookie = cookieString;
+// }
+
+// setCookie("name","hari",360000)
+
 /**
- * Setting cookie function
+ * parsing and getting cookie value
  * @param {*} key 
- * @param {*} value 
- * @param {*} lifeInSeconds 
+ * @returns 
  */
-const setCookie = (key, value, lifeInSeconds) => {
-  const date = new Date();
-  const expiryTime = (date.getTime() + lifeInSeconds);
-  date.setTime(expiryTime);
-  const cookieString = `${key}=${value};expires=${date.toGMTString()}`;
-  console.log("cookieString", cookieString)
-  document.cookie = cookieString;
+const getCookie = (key) => {
+  const allCookies = document.cookie.split(";")
+  console.log("allCookies", allCookies);
+  let value = null;
+  allCookies.forEach((ele) => {
+    const cookieEleTrim = ele.trim();
+    console.log("cookieEleTrim", cookieEleTrim)
+    const cookieEleArray = cookieEleTrim.split("=")
+    console.log("cookieEleArray", cookieEleArray);
+    if (cookieEleArray[0] === key) {
+      value = cookieEleArray[1];
+    }
+  })
+  if(value){
+    return value
+  }
+  return `No cookie with key = ${key}`
 }
 
-setCookie("name","hari",360000)
+console.log(getCookie("name"))
