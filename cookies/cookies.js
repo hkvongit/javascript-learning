fc858523ee6c9887294328edc8039df1051bfc99
hkvongit <brosgiant@gmail.com>
2021-07-04 23:21:54 +0530

    [IMP] - set cookie complete

--- cookies/cookies.js
+++ cookies/cookies.js
@@ -1,16 +1,16 @@
-// /**
-//  * Setting cookie function
-//  * @param {*} key 
-//  * @param {*} value 
-//  * @param {*} lifeInSeconds 
-//  */
-// const setCookie = (key, value, lifeInSeconds) => {
-//   const date = new Date();
-//   const expiryTime = (date.getTime() + lifeInSeconds);
-//   date.setTime(expiryTime);
-//   const cookieString = `${key}=${value};expires=${date.toGMTString()}`;
-//   console.log("cookieString", cookieString)
-//   document.cookie = cookieString;
-// }
+/**
+ * Setting cookie function
+ * @param {*} key 
+ * @param {*} value 
+ * @param {*} lifeInSeconds 
+ */
+const setCookie = (key, value, lifeInSeconds) => {
+  const date = new Date();
+  const expiryTime = (date.getTime() + lifeInSeconds);
+  date.setTime(expiryTime);
+  const cookieString = `${key}=${value};expires=${date.toGMTString()}`;
+  console.log("cookieString", cookieString)
+  document.cookie = cookieString;
+}
 
-// setCookie("name","hari",360000)
+setCookie("name","hari",360000)
